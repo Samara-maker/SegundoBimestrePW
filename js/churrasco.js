9f8ebc726f5d244008b450e75941bdfe61074a64
Samara2006 <samarahespanhol@gmail.com>
2023-06-05 17:24:07 -0400

    att

--- js/churrasco.js
+++ js/churrasco.js
@@ -1,22 +1,38 @@
 const formulario = document.querySelector("#meuForm");
-
 const convidados = document.querySelector("#lista-convidados");
+const alunos= JSON.parse(localStorage.getItem("aunos")) ||[];
 
+    alunos.forEach((elemento)=>
+    {
+        console.log(elemento.aluno,elemento.qtde)
+    });
+    
 formulario.addEventListener("submit",(evento)=>
 {
     evento.preventDefault()/*fechar*/
-    novoElemento(evento.target.elements['aluno'].value, evento.target.elements['qtde'].value)
+    const aluno = evento.target.elements['aluno'];
+    const qtde = evento.target.elements['qtde'];
+    novoElemento(aluno.value,qtde.value);
+    aluno.value="";
+    qtde.value="";
 });
 
 /*novo elemento*/
 
 function novoElemento(aluno,qtde)
 {
-    const novoItem =document.createElement('li')
+    const novoItem =document.createElement('li');
     novoItem.innerHTML = aluno;
-    novoItem.classList.add("item")
-    const numeroItem = document.createElement('span')
-    numeroItem.innerHTML = qtde
-    novoItem.appendChild(numeroItem)
-    convidados.appendChild(novoItem)
+    novoItem.classList.add("item");
+    const numeroItem = document.createElement('span');
+    numeroItem.innerHTML = qtde;
+    novoItem.appendChild(numeroItem);
+    convidados.appendChild(novoItem);
+    
+    const alunoStorage={
+        "aluno":aluno,
+        "qtde":qtde
+    }
+    
+    
 }
